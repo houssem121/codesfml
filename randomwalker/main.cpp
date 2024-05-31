3bd96d348e8cff3628a491730370e91ac09a4c08
houssem121 <hammami290@gmail.com>
2024-05-31 14:52:57 -0400

    randomwalk

---
+++ randomwalker/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+#include "walker.cpp"
+int main()
+{
+    walker *w = new walker();
+    sf::RenderWindow window(sf::VideoMode(1200, 800), "SFML works!");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        w->step();
+        w->display(window);
+        window.display();
+    }
+
+    return 0;
+}
---
+++ randomwalker/walker.cpp
@@ -0,0 +1,45 @@
+#include <SFML/Graphics.hpp>
+#include <random>
+#include <iostream>
+class walker
+{
+    public:
+    int x;
+    int y;
+
+    walker()
+    {
+        x = 800 / 2;
+        y = 400 / 2;
+    }
+    int genRun()
+    {
+        std::random_device rd;                           // Générateur de nombres aléatoires matériel
+        std::mt19937 gen(rd());                          // Générateur de nombres pseudo-aléatoires Mersenne Twister
+        std::uniform_int_distribution<> distrib(1, 6); // Distribution uniforme entre 1 et 4
+
+        int choice = distrib(gen); // Générer le nombre aléatoire
+
+        return choice;
+    }
+    void display(sf::RenderWindow& window)
+    {   std::cout<<"x:"<<x<<"y:"<<y;
+        sf::Vertex point(sf::Vector2f(x, y), sf::Color::White);
+        window.draw(&point, 1, sf::Points);
+    }
+    void step()
+    {
+        int choice = genRun();
+        if(x<=80 || y<=80) {y=x=400;}
+        else if(choice == 0){
+            x++;
+        }else if (choice==1){
+            x--;
+        }else if (choice==2){
+            y--;
+        }else{
+            y++;
+        }
+
+    }
+};

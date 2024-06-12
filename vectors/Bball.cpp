6dc59410406828926d820476e5666864b56b3dcb
houssem121 <houssem290@gmail.com>
2024-06-11 20:24:12 -0400

    hey

--- vectors/Bball.cpp
+++ vectors/Bball.cpp
@@ -1,20 +1,24 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "Bball.hh"
-
+#include <cmath>
 BCball::BCball(){
     location= sf::Vector2f(400.0f, 400.0f);
     velocity=sf::Vector2f(-1/2.0f,1/2.0f);
+    acceleration=sf::Vector2f(-0.01f,0.01f);
 }
 void BCball::update(){
+    if(std::sqrt(std::pow(velocity.x,2)+std::pow(velocity.y,2))<=limite)velocity=velocity+acceleration;
+    std::cout <<"velocity:"<<std::sqrt(std::pow(velocity.x,2)+std::pow(velocity.y,2));
     location=location+velocity;
     std::cout<<location.x;
 }
 void BCball::display(sf::RenderWindow &window)
 {
-    //std::cout << "x:" << location.x << "y:" << location.y;
-    sf::Vertex point(location, sf::Color::White);
-    window.draw(&point, 1, sf::Points);
+    
+    sf::CircleShape circle(5);
+    circle.setPosition(location);
+    window.draw(circle);
 }
 void BCball::checkEdges(){
     if(location.x>1200.0f){
--- vectors/Bball.hh
+++ vectors/Bball.hh
@@ -3,6 +3,8 @@
 class BCball{
     sf::Vector2f location;
     sf::Vector2f velocity;
+    sf::Vector2f acceleration;//added new var
+    int limite = 5;
     public:
     BCball();
     void update();
